9015bf227d4541c5dc681a91f9ea5a243c37dc29
froggogood <omayma.kridiss@gmail.com>
2023-07-19 13:56:05 +0100

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "TypeScript", "Angular", "React", "Vue"],
+        address: {
+            street: "123 Main St",
+            city: "Austin",
+            state: "TX",
+            zip: "78701",
+            country: "USA"
+        }
+    };
+    return member;
+}
